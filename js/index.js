6232d7da3da0335f13e2e8e066a03bb670b1fc01
Victor Zottmann <zottmann.victor@gmail.com>
2022-05-15 01:48:10 +1000

    add only available cars to session storage

--- js/index.js
+++ js/index.js
@@ -4,50 +4,81 @@ $(document).ready(function() {
     let carAvailable = car.availability ? "Available" : "Unavailable";
     let className = car.availability ? "car-available" : "car-unavailable";
   
-    const carModel = car.model.split("-").join("").toLowerCase()
+    const lowerCaseCarModel = car.model.split("-").join("").toLowerCase()
 
     let output = `
       <div class="col">
         <div class="card h-100">
           <img 
-            src="./img/${carModel}.jpeg" 
+            src="./img/${lowerCaseCarModel}.jpeg" 
             class="card-img-top" 
             alt="Image of the car ${car.brand} ${car.model}"
           />
           <div class="card-body">
-            <h5 class="card-title">${car.brand} ${car.model}</h5><br />
-            <p class="card-text">Mileage: <span class="">${car.mileage} Km</span></p>
-            <p class="card-text">Fuel type: <span>${car.fuelType}</span></p>
-            <p class="card-text">Price per day: <span>${car.pricePerDay}</span></p>
+            <h5 class="card-title" id="${car.model}-name">${car.brand} ${car.model}</h5><br />
+            <p class="card-text">Mileage: <span id="${car.model}-mileage">${car.mileage} Km</span></p>
+            <p class="card-text">Fuel type: <span id="${car.model}-fuel">${car.fuelType}</span></p>
+            <p class="card-text">Price per day: <span id="${car.model}-price">${car.pricePerDay}</span></p>
             <p class="card-text"><span class="${className}">${carAvailable}</span></p>
-            <button id="btn-${carModel}" class="btn-reserve">Reserve</button>
+            <button id="btn-${car.model}" class="btn-reserve">Reserve</button>
           </div>
         </div>
       </div>
     `;
     $('.row').append(output);
 
-    $(`#btn-${carModel}`).click(() => { 
-      if (!car.availability) {
-        console.log("You cannot reserve this car at the moment");
-      } 
+    $(`#btn-${car.model}`).click(() => { 
       cartNumbers();
+
+      let carName = $(`#${car.model}-name`).text();
+      let carMileage = $(`#${car.model}-mileage`).text();
+      let carFuel = $(`#${car.model}-fuel`).text();
+      let carPrice = $(`#${car.model}-price`).text();
+      let carStatus = car.availability;
+
+      let carObject = {
+        name: carName,
+        mileage: carMileage,
+        fuel: carFuel,
+        price: parseFloat(carPrice),
+        inCart: 0,
+      }
+
+      if (carStatus) {
+        storeItem(carObject);
+      } else {
+        console.log("You can't add this car");
+      }
+
+
     });
   }
 
+  function storeItem(carObject) {
+    let cartItems = sessionStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
 
-  let products = [
-    {
-      name: "Grey Shirt",
-      tag: "greyshirt",
-      price: 15,
-      inCart: 0,
+    if (cartItems != null) {
+      if (cartItems[carObject.name] == undefined) {
+        cartItems = {
+          ...cartItems,
+          [carObject.name]: carObject
+        }
+      }
+      cartItems[carObject.name].inCart += 1;
+    } else {
+      carObject.inCart = 1;
+      cartItems = {
+        [carObject.name]: carObject
+      }
     }
-  ]
 
-  
+    sessionStorage.setItem("productsInCart", JSON.stringify(cartItems));
+  }
+
+
   function onLoadCartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers'); // returns a string
+    let productNumbers = sessionStorage.getItem('cartNumbers'); // returns a string
     if (productNumbers) {
       $('.cart-count').text(productNumbers)
     }
@@ -56,18 +87,18 @@ $(document).ready(function() {
 
 
   function cartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers'); // returns a string
+    let productNumbers = sessionStorage.getItem('cartNumbers'); // returns a string
     productNumbers = parseInt(productNumbers);
     
     if (productNumbers) {
-      localStorage.setItem('cartNumbers', productNumbers += 1);
+      sessionStorage.setItem('cartNumbers', productNumbers += 1);
       $('.cart-count').text(productNumbers)
     } else {
-      localStorage.setItem('cartNumbers', 1);
+      sessionStorage.setItem('cartNumbers', 1);
       $('.cart-count').text(1)
     }
   }
-
+  
   
   (function getCars() {
     try {
